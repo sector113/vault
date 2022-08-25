84407f279f5f9957a7448ffed7d60d287e889368
robmonte <17119716+robmonte@users.noreply.github.com>
2022-08-24 22:35:14 -0500

    Add integration test using postgres multihost connection string

--- helper/testhelpers/postgresql/postgresqlhelper.go
+++ helper/testhelpers/postgresql/postgresqlhelper.go
@@ -11,42 +11,65 @@ import (
 	"github.com/hashicorp/vault/helper/testhelpers/docker"
 )
 
-func PrepareTestContainer(t *testing.T, version string) (func(), string) {
-	return prepareTestContainer(t, version, "secret", "database")
+func PrepareTestContainer(t *testing.T, version string) (*docker.Runner, func(), string, string) {
+	return prepareTestContainer(t, "postgres", "postgres", version, "secret", "POSTGRES_DB=database", true, false, nil)
 }
 
-func PrepareTestContainerWithPassword(t *testing.T, version, password string) (func(), string) {
-	return prepareTestContainer(t, version, password, "database")
+func PrepareTestContainerWithPassword(t *testing.T, version, password string) (*docker.Runner, func(), string, string) {
+	return prepareTestContainer(t, "postgres", "postgres", version, password, "POSTGRES_DB=database", true, false, nil)
 }
 
-func prepareTestContainer(t *testing.T, version, password, db string) (func(), string) {
+func PrepareTestContainerRepmgr(t *testing.T, name, version string, envVars []string) (*docker.Runner, func(), string, string) {
+	return prepareTestContainer(t, name, "bitnami/postgresql-repmgr", version, "secret", "", false, true, envVars)
+}
+
+func StopContainer(t *testing.T, ctx context.Context, runner *docker.Runner, containerID string) {
+	err := runner.Stop(ctx, containerID)
+	if err != nil {
+		t.Fatalf("Could not stop docker Postgres: %s", err)
+	}
+}
+
+func RestartContainer(t *testing.T, ctx context.Context, runner *docker.Runner, containerID string) {
+	err := runner.Restart(ctx, containerID)
+	if err != nil {
+		t.Fatalf("Could not restart docker Postgres: %s", err)
+	}
+}
+
+func prepareTestContainer(t *testing.T, name, repo, version, password, db string, addSuffix, doNotAutoRemove bool, envVars []string) (*docker.Runner, func(), string, string) {
 	if os.Getenv("PG_URL") != "" {
-		return func() {}, os.Getenv("PG_URL")
+		return nil, func() {}, "", os.Getenv("PG_URL")
 	}
 
 	if version == "" {
 		version = "11"
 	}
 
-	runner, err := docker.NewServiceRunner(docker.RunOptions{
-		ImageRepo: "postgres",
-		ImageTag:  version,
-		Env: []string{
-			"POSTGRES_PASSWORD=" + password,
-			"POSTGRES_DB=" + db,
-		},
-		Ports: []string{"5432/tcp"},
-	})
+	runOpts := docker.RunOptions{
+		ContainerName:   name,
+		ImageRepo:       repo,
+		ImageTag:        version,
+		Env:             envVars,
+		NetworkID:       "postgres-repmgr-net",
+		Ports:           []string{"5432/tcp"},
+		DoNotAutoRemove: doNotAutoRemove,
+	}
+	if repo == "bitnami/postgresql-repmgr" {
+		runOpts.NetworkID = os.Getenv("POSTGRES_MULTIHOST_NET")
+	}
+
+	runner, err := docker.NewServiceRunner(runOpts)
 	if err != nil {
 		t.Fatalf("Could not start docker Postgres: %s", err)
 	}
 
-	svc, err := runner.StartService(context.Background(), connectPostgres(password))
+	svc, containerID, err := runner.StartService(context.Background(), addSuffix, connectPostgres(password))
 	if err != nil {
 		t.Fatalf("Could not start docker Postgres: %s", err)
 	}
 
-	return svc.Cleanup, svc.Config.URL().String()
+	return runner, svc.Cleanup, svc.Config.URL().String(), containerID
 }
 
 func connectPostgres(password string) docker.ServiceAdapter {
--- plugins/database/postgresql/postgresql_test.go
+++ plugins/database/postgresql/postgresql_test.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"os"
 	"strings"
 	"testing"
 	"time"
 
 	"github.com/hashicorp/vault/sdk/database/helper/dbutil"
 
+	"github.com/hashicorp/vault/helper/testhelpers/docker"
 	"github.com/hashicorp/vault/helper/testhelpers/postgresql"
 	"github.com/hashicorp/vault/sdk/database/dbplugin/v5"
 	dbtesting "github.com/hashicorp/vault/sdk/database/dbplugin/v5/testing"
@@ -18,7 +20,7 @@ import (
 )
 
 func getPostgreSQL(t *testing.T, options map[string]interface{}) (*PostgreSQL, func()) {
-	cleanup, connURL := postgresql.PrepareTestContainer(t, "13.4-buster")
+	_, cleanup, connURL, _ := postgresql.PrepareTestContainer(t, "13.4-buster")
 
 	connectionDetails := map[string]interface{}{
 		"connection_url": connURL,
@@ -64,7 +66,7 @@ func TestPostgreSQL_InitializeWithStringVals(t *testing.T) {
 }
 
 func TestPostgreSQL_Initialize_ConnURLWithDSNFormat(t *testing.T) {
-	cleanup, connURL := postgresql.PrepareTestContainer(t, "13.4-buster")
+	_, cleanup, connURL, _ := postgresql.PrepareTestContainer(t, "13.4-buster")
 	defer cleanup()
 
 	dsnConnURL, err := dbutil.ParseURL(connURL)
@@ -895,7 +897,7 @@ func TestUsernameGeneration(t *testing.T) {
 }
 
 func TestNewUser_CustomUsername(t *testing.T) {
-	cleanup, connURL := postgresql.PrepareTestContainer(t, "13.4-buster")
+	_, cleanup, connURL, _ := postgresql.PrepareTestContainer(t, "13.4-buster")
 	defer cleanup()
 
 	type testCase struct {
@@ -989,4 +991,127 @@ func TestNewUser_CustomUsername(t *testing.T) {
 			require.Regexp(t, test.expectedRegex, newUserResp.Username)
 		})
 	}
+}
+
+// This is a long-running integration test which tests the functionality of Postgres's multi-host
+// connection strings. It uses two Postgres containers preconfigured with Replication Manager
+// provided by Bitnami. This test currently does not run in CI and must be run manually. This is
+// due to the test length, as it requires multiple sleep calls to ensure cluster setup and
+// primary node failover occurs before the test steps continue.
+//
+// To run the test, set the environment variable POSTGRES_MULTIHOST_NET to the value of
+// a docker network you've preconfigured, e.g.
+// `docker network create -d bridge postgres-repmgr`
+// `export POSTGRES_MULTIHOST_NET=postgres-repmgr`
+func TestPostgreSQL_Repmgr(t *testing.T) {
+	_, exists := os.LookupEnv("POSTGRES_MULTIHOST_NET")
+	if !exists {
+		t.Skipf("POSTGRES_MULTIHOST_NET not set, skipping test")
+	}
+
+	db0, runner0, container0ID := testPostgreSQL_Repmgr_Container(t, "psql-repl-0")
+
+	db1, _, _ := testPostgreSQL_Repmgr_Container(t, "psql-repl-1")
+
+	ctx0, cancel0 := context.WithTimeout(context.Background(), 300*time.Second)
+	ctx1, cancel1 := context.WithTimeout(context.Background(), 300*time.Second)
+	defer cancel0()
+	defer cancel1()
+	time.Sleep(20 * time.Second)
+
+	_, err := db0.NewUser(ctx0, dbplugin.NewUserRequest{
+		Statements: dbplugin.Statements{
+			Commands: []string{
+				`CREATE ROLE "ro" NOINHERIT;
+				GRANT SELECT ON ALL TABLES IN SCHEMA public TO "ro";`,
+			},
+		},
+	})
+	if err != nil {
+		t.Fatalf("no error expected, got: %s", err)
+	}
+
+	err = testPostgreSQL_Repmgr_AddUser(t, ctx0, db0)
+	if err != nil {
+		t.Fatalf("no error expected, got: %s", err)
+	}
+
+	err = testPostgreSQL_Repmgr_AddUser(t, ctx1, db1)
+	if err.Error() != "failed to execute query: ERROR: cannot execute CREATE ROLE in a read-only transaction (SQLSTATE 25006)" {
+		t.Fatalf("expected error was not received, got: %s", err)
+	}
+
+	err = runner0.Stop(ctx0, container0ID)
+	// err = runner0.Restart(ctx0, container0ID)
+	if err != nil {
+		t.Fatalf("failed to stop container '%s': %s", container0ID, err)
+	}
+
+	err = testPostgreSQL_Repmgr_AddUser(t, ctx0, db0)
+	if !strings.HasPrefix(err.Error(), "unable to start transaction: failed to connect to `host=127.0.0.1 user=postgres database=postgres`: dial error (dial tcp 127.0.0.1") {
+		t.Fatalf("expected error was not received, got: %s", err)
+	}
+
+	time.Sleep(60 * time.Second)
+
+	err = testPostgreSQL_Repmgr_AddUser(t, ctx1, db1)
+	if err != nil {
+		t.Fatalf("no error expected, got: %s", err)
+	}
+
+	err = runner0.Restart(ctx0, container0ID)
+	if err != nil {
+		t.Fatalf("failed to restart container '%s': %s", container0ID, err)
+	}
+	time.Sleep(20 * time.Second)
+
+	err = testPostgreSQL_Repmgr_AddUser(t, ctx1, db1)
+	if err != nil {
+		t.Fatalf("no error expected, got: %s", err)
+	}
+}
+
+func testPostgreSQL_Repmgr_Container(t *testing.T, name string) (*PostgreSQL, *docker.Runner, string) {
+	envVars := []string{
+		"REPMGR_PARTNER_NODES=psql-repl-0,psql-repl-1",
+		"REPMGR_PRIMARY_HOST=psql-repl-0",
+		"REPMGR_PASSWORD=repmgrpass",
+		"POSTGRESQL_PASSWORD=secret",
+		"REPMGR_NODE_NAME=" + name,
+		"REPMGR_NODE_NETWORK_NAME=" + name,
+	}
+
+	runner, cleanup, connURL, containerID := postgresql.PrepareTestContainerRepmgr(t, name, "13.4.0", envVars)
+	t.Cleanup(cleanup)
+
+	connectionDetails := map[string]interface{}{
+		"connection_url": connURL,
+	}
+	req := dbplugin.InitializeRequest{
+		Config:           connectionDetails,
+		VerifyConnection: true,
+	}
+	db := new()
+	dbtesting.AssertInitialize(t, db, req)
+	if !db.Initialized {
+		t.Fatal("Database should be initialized")
+	}
+	if err := db.Close(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	return db, runner, containerID
+}
+
+func testPostgreSQL_Repmgr_AddUser(t *testing.T, ctx context.Context, db *PostgreSQL) error {
+	_, err := db.NewUser(ctx, dbplugin.NewUserRequest{
+		Statements: dbplugin.Statements{
+			Commands: []string{
+				`CREATE ROLE "{{name}}" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}' INHERIT;
+				GRANT ro TO "{{name}}";`,
+			},
+		},
+	})
+
+	return err
 }
